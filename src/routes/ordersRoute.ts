76b3861c875634c6798153621a199dfab7d43dea
LuisLaporta <lui.laporta99@gmail.com>
2022-12-14 18:53:29 -0300

    Creating Route Orders

---
+++ src/routes/ordersRoute.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import * as ordersController from '../controllers/ordersController';
+
+const router = Router();
+
+router.get('/', ordersController.getAll);
+
+export default router;
