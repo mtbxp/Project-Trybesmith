3268711e3f1c9c2c2da5af16345bb15061fe71bc
andressaribeiroo <andressaclecia.ribeiro@gmail.com>
2022-12-14 16:45:17 -0300

    feat: add and create router for users

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import product from './router/productRoute';
+import users from './router/userRoute';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', product);
+app.use('/users', users);
+
 
 export default app;
---
+++ src/router/userRoute.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import UserController from '../controller/userController';
+
+const router = Router();
+
+const userController = new UserController();
+
+router.post('/', userController.create);
+
+export default router;
