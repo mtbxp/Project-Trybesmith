b88f9d1c3246840bf150fa8a58300acba23a89c6
Gabriela Moura <gabrieladaniel49@hotmail.com>
2022-12-14 16:31:35 -0300

    :sparkles: requisito 01

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import ProductService from '../services/product.service';
+
+export default class ProductsController {
+  constructor(private productService = new ProductService()) {}
+
+  public create = async (req: Request, res: Response) => {
+    const product = req.body;
+
+    const productCreated = await this.productService.create(product);
+    return res.status(201).json(productCreated);
+  };
+}
