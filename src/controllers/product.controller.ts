0d96a66073733a8ff7f9b233cff0af4d7bdb0f36
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-14 17:05:54 -0300

    cria controller para listar todods os produtos

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,12 +3,22 @@ import * as ProductService from '../services/product.service';
 import { TProduct } from '../types';
 
 export async function create(req: Request, res: Response) {
-  const product = req.body as TProduct;
-  const newProduct = await ProductService.create(product);
+  try {
+    const product = req.body as TProduct;
+    const newProduct = await ProductService.create(product);
 
-  return res.status(201).json(newProduct);
+    return res.status(201).json(newProduct);
+  } catch (err:unknown) {
+    console.log(err);
+  }
 }
 
-export function getAll() {
+export async function getAll(_req: Request, res: Response) {
+  try {
+    const products = await ProductService.getAll();
 
+    return res.status(200).json(products);
+  } catch (err:unknown) {
+    console.log(err);
+  }
 }
