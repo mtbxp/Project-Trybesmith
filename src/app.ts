9e7d821b30b5b8b5dca7be7e2d07965d59fa3645
Guyddo <guyddogl@gmail.com>
2022-12-14 19:11:57 -0300

    Add login route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import productsRoute from './routes/productsRoute';
 import usersRoute from './routes/usersRoute';
 import ordersRoute from './routes/ordersRoute';
+import loginRoute from './routes/loginRoute';
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use('/products', productsRoute);
 app.use('/users', usersRoute);
 app.use('/orders', ordersRoute);
+app.use('/login', loginRoute);
 
 export default app;
