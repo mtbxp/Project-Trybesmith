24e429c480527aa4ab2f415705d94bc94196865e
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-14 17:09:19 -0300

    adiciona router para endpoint /products para listar produtos

--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -3,6 +3,8 @@ import * as ProductController from '../controllers/product.controller';
 
 const productRouter = Router();
 
+productRouter.get('/products', ProductController.getAll);
+
 productRouter.post('/products', ProductController.create);
 
 export default productRouter;
