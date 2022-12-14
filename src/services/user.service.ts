ac223383cc5822844924448f726470b04bfb3033
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-14 18:34:29 -0300

    cria service para criar usuari e gerar um token

---
+++ src/services/user.service.ts
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import * as UserModel from '../models/user.model';
+import { TUser } from '../types';
+
+const secret = process.env.JWT_SECRET || 'secret';
+
+const jwtConfig:object = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
+
+export async function create(user:TUser):Promise<object> {
+  const payload = await UserModel.create(user);
+
+  const token = jwt.sign(payload, secret, jwtConfig);
+
+  return { token };
+}
+
+export function getAll() {
+
+}
