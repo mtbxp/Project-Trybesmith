123e929cba770206c629d0e69799207379bbda1d
Gabriela Moura <gabrieladaniel49@hotmail.com>
2022-12-14 20:33:25 -0300

    :sparkles: requisito 07

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import UsersController from '../controllers/user.controller';
+import UsernameMiddleware from '../middleware/username.middleware';
+import VocationMiddleware from '../middleware/vocation.middleware';
+import LevelMiddleware from '../middleware/level.middleware';
+import PasswordMiddleware from '../middleware/password.middleware';
 
 const router = Router();
 
 const userController = new UsersController();
+const usernameMiddleware = new UsernameMiddleware();
+const vocationMiddleware = new VocationMiddleware();
+const levelMiddleware = new LevelMiddleware();
+const passwordMiddleware = new PasswordMiddleware();
 
-router.post('/users', userController.create);
+router.post('/users', usernameMiddleware.username, vocationMiddleware.vocation, levelMiddleware
+  .level, passwordMiddleware.password, userController.create);
 
 export default router;
