bdc78589616b54b9be4607eea26526d9229de9ed
LuisLaporta <lui.laporta99@gmail.com>
2022-12-14 16:09:33 -0300

    Addition of Get Route in Products Routes

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -4,5 +4,6 @@ import * as productsController from '../controllers/productsController';
 const router = Router();
 
 router.post('/', productsController.insert);
+router.get('/', productsController.getAll);
 
 export default router;
