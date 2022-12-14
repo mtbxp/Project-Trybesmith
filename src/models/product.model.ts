6f4959afefacc90acd2c84e02288375b86cab8e5
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-14 16:38:54 -0300

    cria conexão com BD, e cria models para cria um produto e buscar um produto pelo id

---
+++ src/models/product.model.ts
@@ -0,0 +1,18 @@
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
+import connection from './connection';
+import { TProduct } from '../types';
+
+export async function create(product: TProduct):Promise<ResultSetHeader> {
+  const [result] = await connection.execute<ResultSetHeader>(
+    'INSERT INTO Trybesmith.products (name, amount) VALUE (?, ?)',
+    [...Object.values(product)],
+  );
+
+  return result;
+}
+
+export async function getById(id:number):Promise<TProduct | null> {
+  const [[result]] = await connection.execute<RowDataPacket[] &
+  TProduct[]>('SELECT * FROM Trybesmith.products WHERE id = ?', [id]);
+  return result;
+}
