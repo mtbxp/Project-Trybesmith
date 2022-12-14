2de0014b0fa85d82e18870fa9212183d87be94b7
Lucas Israel <lisraelalves92@gmail.com>
2022-12-14 16:03:27 -0300

    add: product service

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -4,14 +4,14 @@ import connection from './connection';
 
 const productTable = 'Trybesmith.products';
 
-export async function create(product: Product): Promise<IProduct> {
+export async function create(product: Product): Promise<IProduct[]> {
   const { amount, name } = product;
   const query = `INSERT INTO ${productTable} (name, amount) VALUES (?, ?)`;
   const values = [name, amount];
   const [result] = await connection.execute<ResultSetHeader>(query, values);
   const { insertId: id } = result;
   
-  return { id, name, amount };
+  return [{ id, name, amount }];
 }
 
 export async function getAll(): Promise<IProduct[]> {
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -2,6 +2,11 @@ import { Product, ProductService } from '../interfaces';
 import * as productModel from '../models/product.model';
 
 export async function create(product: Product): Promise<ProductService> {
-  const result = await productModel.create(product);
+  const [result] = await productModel.create(product);
   return { status: 201, result };
+}
+
+export async function getAll(): Promise<ProductService> {
+  const [result] = await productModel.getAll();
+  return { status: 200, result };
 }
