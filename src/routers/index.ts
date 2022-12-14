6920109b222f87add64d7aa77d5bf2406dc967d1
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-14 18:36:38 -0300

    cria router para endpoint /users para criar um usuario gerando um token para validar

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import productRouter from './product.router';
+import userRouter from './user.router';
 
 const router = Router();
 
+router.use(userRouter);
 router.use(productRouter);
 
 export default router;
