d335f5ad3e408718a4abb26cf989266ee07e86d1
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-12-14 16:03:43 -0300

    Camada Service implementada.

---
+++ src/services/products.service.ts
@@ -0,0 +1,14 @@
+import { Tproducts } from '../models/interfaces';
+import ProductsModel from '../models/products.model';
+
+const addNewProduct = async (newProduct: Tproducts): Promise<Tproducts> => {
+  const idNewProduct: number = await ProductsModel.insertNewProduct(newProduct);
+  const newUser: Tproducts = {
+    id: idNewProduct,
+    name: newProduct.name,
+    amount: newProduct.amount,
+  };  
+  return newUser;
+};
+
+export default { addNewProduct };
