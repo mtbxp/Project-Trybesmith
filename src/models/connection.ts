c73aa6a273c6835d8e8c54a161d881b6a7294539
Guyddo <guyddogl@gmail.com>
2022-12-14 15:55:35 -0300

    Cria a conexão com o banco de dados

---
+++ src/models/connection.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+
+dotenv.config();
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB || 'Trybesmith',
+});
+
+export default connection;
