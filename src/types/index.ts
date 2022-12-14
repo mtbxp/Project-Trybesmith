b0d483a61c20228769f1eba6fc39ac06454e9a45
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-14 18:31:15 -0300

    cria type para token

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,23 @@ export type TProduct = {
   name: string,
   amount: string
 };
+
+export type TUser = {
+  id?: number;
+  username: string,
+  vocation: string,
+  level: number,
+  password?: string
+};
+
+export type IToken = {
+  payload: {
+    id?: number;
+    username: string,
+    vocation: string,
+    level: number,
+    password?: string
+  };
+  iat: number;
+  exp: number;
+};
