4bf5da3512dbfa87206a75429f16ebcf3a45ba58
Lucas Israel <lisraelalves92@gmail.com>
2022-12-14 15:48:08 -0300

    add: product model

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,13 +1,22 @@
-import { ResultSetHeader } from 'mysql2/promise';
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { Product, IProduct } from '../interfaces/index';
 import connection from './connection';
 
+const productTable = 'Trybesmith.products';
+
 export async function create(product: Product): Promise<IProduct> {
   const { amount, name } = product;
-  const query = 'INSERT INTO Trybesmith.products (name, amount) VALUES (?, ?)';
+  const query = `INSERT INTO ${productTable} (name, amount) VALUES (?, ?)`;
   const values = [name, amount];
   const [result] = await connection.execute<ResultSetHeader>(query, values);
   const { insertId: id } = result;
   
   return { id, name, amount };
+}
+
+export async function getAll(): Promise<IProduct[]> {
+  const query = `SELECT * FROM ${productTable}`;
+  const [result] = await connection.execute<RowDataPacket[] & IProduct>(query);
+
+  return [result];
 }
