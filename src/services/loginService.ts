ae305efdbab132cb53483a733a7a91917eade892
Guyddo <guyddogl@gmail.com>
2022-12-14 20:40:02 -0300

    Refatora logUserIn

--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -1,15 +1,15 @@
 import usersModel from '../models/usersModel';
 import token from '../utils/token';
 import { Login, Response } from '../interfaces/Login';
-import { NewUser } from '../interfaces/User';
+import { FoundUser } from '../interfaces/User';
 
 const logUserIn = async (body: Login): Promise<Response> => {
-  const findUser: NewUser = await usersModel.getUser(body.username);
-  if (!findUser || findUser.password !== body.password) { 
+  const userFound: FoundUser = await usersModel.getUser(body.username);
+  if (!userFound || userFound.password !== body.password) {
     return { status: 401, message: 'Username or password invalid' };
   }
-  const user = { _password: findUser.password, ...findUser };
-  const userToken = token.generateToken(user);
+  const { password, ...userWithoutPassword } = userFound;
+  const userToken = token.generateToken(userWithoutPassword);
   return { status: 200, message: userToken };
 };
 
