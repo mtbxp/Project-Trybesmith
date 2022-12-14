9829c13cb712cca093da606148db4e610d7dba82
Lucas Israel <lisraelalves92@gmail.com>
2022-12-14 15:11:06 -0300

    add: product.model and IProduct interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,7 @@ export interface Product {
   name: string;
   ammount: number;
 }
+
+export interface IProduct extends Product {
+  id: number;
+}
---
+++ src/models/product.model.ts
@@ -0,0 +1,17 @@
+import { ResultSetHeader } from 'mysql2/promise';
+import { Product, IProduct } from '../interfaces/index';
+import connection from './connection';
+
+export async function create(product: Product): Promise<IProduct> {
+  const { ammount, name } = product;
+  const query = 'INSERT INTO Trybesmith (name, ammount) VALUES (?, ?)';
+  const values = [name, ammount];
+  const [result] = await connection.execute<ResultSetHeader>(query, values);
+  const { insertId: id } = result;
+  
+  return { id, name, ammount };
+}
+
+export async function getAll() {
+
+}
