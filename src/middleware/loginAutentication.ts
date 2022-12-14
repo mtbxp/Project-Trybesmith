4222cc68ff24012e580d1df8a9dd7ce984d627a1
Luis Rowe <luisfelipe.apoloniorowe@gmail.com>
2022-12-14 19:39:00 -0300

    Req 7

--- src/middleware/loginAutentication.ts
+++ src/middleware/loginAutentication.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
+import { Login } from '../types/types';
 
 const loginValidation = async (req: Request, res: Response, next: NextFunction) => {
-  const { username, password } = req.body;
-  if (!username) return res.status(400).json({ message: '"username" is required' });
-  if (!password) return res.status(400).json({ message: '"password" is required' });
+  const login: Login = req.body;
+  if (!login.username) return res.status(400).json({ message: '"username" is required' });
+  if (!login.password) return res.status(400).json({ message: '"password" is required' });
   next();
 };
 
--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,8 +1,21 @@
 import { Router } from 'express';
 import usersController from '../controllers/users.controller';
+import { 
+  levelValidation,
+  passwordValidation,
+  usernameValidation,
+  vocationValidation,
+} from '../middleware/validateUser';
 
 const router = Router();
 
-router.post('/', usersController.create);
+router.post(
+  '/', 
+  usernameValidation,
+  vocationValidation,
+  levelValidation,
+  passwordValidation,
+  usersController.create,
+);
 
 export default router;
