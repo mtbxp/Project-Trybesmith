5431f1d0798c5addab5cbb983c67f888475990d9
Guyddo <guyddogl@gmail.com>
2022-12-15 10:32:58 -0300

    Valida o campos com Joi

--- src/middlewares/validateLogin.ts
+++ src/middlewares/validateLogin.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
+import {  usernameSchema, passwordSchema } from '../utils/inputsValidations'
 
 const validateLogin = (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body;
-  if (!username) return res.status(400).json({ message: '"username" is required' });
-  if (!password) return res.status(400).json({ message: '"password" is required' });
+  const validateUsername = usernameSchema.validate(username);
+  const validatePassword = passwordSchema.validate(password);
+  if (validateUsername.error || validatePassword.error) {
+    const error = validateUsername.error || validatePassword.error;
+    return res.status(400).json({ message: error?.details[0].message });
+  }
   next();
 };
 
