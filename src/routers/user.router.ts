5160fe0554390d9e5d1586d7919be3d645f431af
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-15 11:01:02 -0300

    adiciona router para endpoint /login para gerar um token

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import * as UserController from '../controllers/user.controller';
+import login from '../controllers/login';
+import validLogin from '../middlewares/validLogin';
 
 const userRouter = Router();
 
 userRouter.post('/users', UserController.create);
+userRouter.post('/login', validLogin, login);
 
 export default userRouter;
