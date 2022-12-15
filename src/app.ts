1d957239fdbda51d15d0fab8cc4f70312b118c9c
Danilo Isaac <daniloisaac942@gmail.com>
2022-12-15 18:33:18 -0300

    feat: adicionei a rota orders no app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import products from './routes/products.routes';
 import users from './routes/user.routes';
+import orders from './routes/orders.routes';
 
 const app = express();
 // vqv
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/products', products);
 app.use('/users', users);
+app.use('/orders', orders);
 
 export default app;
