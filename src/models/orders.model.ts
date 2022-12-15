81fd74028dafda966d6f74f8277af400a47b1eb7
Vinny-knzts <viniciusmasouza@hotmail.com>
2022-12-14 21:34:29 -0400

    Completa req 04.

---
+++ src/models/orders.model.ts
@@ -0,0 +1,22 @@
+import { Pool } from 'mysql2/promise';
+import Order from '../interfaces/orders.interface';
+
+export default class OrderModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<Order[]> {
+    const result = await this.connection.execute(
+      'SELECT orders.id, orders.user_id AS userId, JSON_ARRAYAGG(products.id) AS productsIds'
+      + ' FROM Trybesmith.orders AS orders'
+      + ' INNER JOIN Trybesmith.products AS products'
+      + ' WHERE orders.id = products.order_id'
+      + ' GROUP BY orders.id',
+    );
+    const [rows] = result;
+    return rows as Order[];
+  }
+}
