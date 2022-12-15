5afe837fb1bf609c8e3ab13c04cb222a4853575b
Danilo Isaac <daniloisaac942@gmail.com>
2022-12-15 15:12:50 -0300

    feat: adicionei rota user no app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import products from './routes/products.routes';
+import users from './routes/user.routes';
 
 const app = express();
 // vqv
 app.use(express.json());
 
 app.use('/products', products);
+app.use('/users', users);
 
 export default app;
