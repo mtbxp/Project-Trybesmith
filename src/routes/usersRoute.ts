136282f75170e4c5dd9b56467b397075b38e3312
Guyddo <guyddogl@gmail.com>
2022-12-15 10:33:31 -0300

    Adiciona middleware de validação

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import userController from '../controllers/userController';
+import validateSignUp from '../middlewares/validateSignUp';
 
 const routers = Router();
 
-routers.post('/', userController.addUser);
+routers.post('/', validateSignUp, userController.addUser);
 
 export default routers;
