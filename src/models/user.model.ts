c6ba9d5485e6d90e5e3440a71169885f50b6ac8f
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-15 10:55:45 -0300

    cria model para buscar valores para validar o password e name do usuario

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,4 @@
-import { ResultSetHeader } from 'mysql2/promise';
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { TUser } from '../types';
 
 import connection from './connection';
@@ -12,6 +12,9 @@ export async function create(user: TUser):Promise<TUser> {
   return user;
 }
 
-export function getAll() {
-
+export async function getByUser(name:string):Promise<TUser> {
+  const [[result]] = await connection
+    .execute<RowDataPacket[0] & TUser>(`SELECT * FROM Trybesmith.users 
+  WHERE username = ?`, [name]);
+  return result;
 }
