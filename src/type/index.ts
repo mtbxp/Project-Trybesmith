895cbfdf91d4d935f70872efb8e62957842cdcb4
Danilo Isaac <daniloisaac942@gmail.com>
2022-12-15 15:12:11 -0300

    feat: criei Tuser no diretorio types

--- src/type/index.ts
+++ src/type/index.ts
@@ -3,3 +3,11 @@ export type TProducts = {
   name: string
   amount: string
 };
+
+export type TUser = {
+  id?: number 
+  username: string,
+  vocation: string,
+  level: number,
+  password: string,
+};
