0dae96ecba58177c29468658709ff4d93202adbe
Miguel Ângelo <miguelcg958@gmail.com>
2022-12-15 15:12:11 -0300

    requisito 7 finalizado

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -3,15 +3,33 @@ import { Request, Response } from 'express';
 
 import * as usersService from '../services/users.service';
 
+const createUserSchema = Joi.object({
+  level: Joi.number().min(1).required(),
+  password: Joi.string().min(8).required(),
+  username: Joi.string().min(3).required(),
+  vocation: Joi.string().min(3).required(),
+});
+
 const loginSchema = Joi.object({
   username: Joi.string().required(),
   password: Joi.string().required(),
 });
 
 async function createUser(request: Request, response: Response): Promise<Response> {
-  const { level, password, username, vocation } = request.body;
+  const { value, error } = createUserSchema.validate(request.body);
 
-  const token = await usersService.createUser({ level, password, username, vocation });
+  if (error) {
+    const checks: RegExp[] = [/must be a/, /must be greater than/, /length must be at least/];
+    let status = 400;
+
+    if (checks.some((reg) => reg.test(error.message))) {
+      status = 422;
+    }
+
+    return response.status(status).json({ message: error.message });
+  }
+
+  const token = await usersService.createUser(value);
 
   return response.status(201).json({ token });
 }
