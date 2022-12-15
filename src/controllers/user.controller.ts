a2a2058e6de1b65a8ac0d778c63c0ff3a222ad65
Lucas Israel <lisraelalves92@gmail.com>
2022-12-15 10:02:16 -0300

    done req7

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from 'express';
 import { ServiceError, TokenService } from '../interfaces';
 import * as userService from '../services/user.service';
-import userCheck from '../middlewares/userVerification';
+import { userCheck, userCreation } from '../middlewares/userVerification';
 
 export async function create(req: Request, res: Response) {
   const { username, vocation, level, password } = req.body;
+  const { error: validationError } = userCreation(req.body);
+
+  const statusCode = validationError && /"\w.*" is required/.test(validationError?.message)
+    ? 400 : 422;
+
+  if (validationError) return res.status(statusCode).json({ message: validationError.message });
+
   const { status, error, result } = await userService
     .create({ username, vocation, level, password });
 
--- src/middlewares/userVerification.ts
+++ src/middlewares/userVerification.ts
@@ -1,17 +1,34 @@
 import Joi from 'joi';
 
-const userCheck = (body: object) => {
+export const userCheck = (body: object) => {
   const schema = Joi.object({
-    username: Joi.string().min(3).required().messages({
-      'string.min': '"username" length greather than 2',
+    username: Joi.string().min(2).required().messages({
+      'string.min': '"username" length must be at least 2 characters long',
       'any.required': '"username" is required',
     }),
-    password: Joi.string().min(3).required().messages({
-      'string.min': '"password" length greather than 2',
+    password: Joi.string().min(8).required().messages({
+      'string.min': '"password" length must be at least 8 characters long',
       'any.required': '"password" is required',
     }),
   });
   return schema.validate(body);
 };
 
-export default userCheck;
+export const userCreation = (body: object) => Joi.object({
+  username: Joi.string().min(3).required().messages({
+    'string.min': '"username" length must be at least 3 characters long',
+    'any.required': '"username" is required',
+  }),
+  password: Joi.string().min(8).required().messages({
+    'string.min': '"password" length must be at least 8 characters long',
+    'any.required': '"password" is required',
+  }),
+  vocation: Joi.string().min(3).required().messages({
+    'string.min': '"vocation" length must be at least 3 characters long',
+    'any.required': '"vocation" is required',
+  }),
+  level: Joi.number().min(1).required().messages({
+    'number.min': '"level" must be greater than or equal to 1',
+    'any.required': '"level" is required',
+  }),
+}).validate(body);
