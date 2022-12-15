b25746e830df256276f47069e50fb99917e786ac
Pedro <pedro_hasse@yahoo.com.br>
2022-12-15 13:02:55 -0300

    feat: 🎸 Requisito 1 ok

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -2,13 +2,13 @@ import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import { Product, ProductRequest } from '../interfaces/products.interface';
 import connection from './connection';
 
-export const registerProductModel = async ({ name, amount }: ProductRequest): Promise<number> => {
+export const registerProductModel = async ({ name, amount }: ProductRequest): Promise<Product> => {
   const query = 'INSERT INTO Trybesmith.products (name, amount) VALUES (?, ?)';
   const [result] = await connection.execute<ResultSetHeader>(
     query,
     [name, amount],
   );
-  return result.insertId;
+  return { id: result.insertId, name, amount };
 };
 
 export const getAllProductsModel = async (): Promise<Product[]> => {
