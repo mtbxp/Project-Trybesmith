94a8787d2276719f1a60fb7fc936cea0b1c78610
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-12-15 10:27:38 -0300

    Requisito 6

--- src/routes/productRouter.ts
+++ src/routes/productRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import * as productController from '../controllers/productController';
+import validateProduct from '../middlewares/validateProduct';
 
 const router = Router();
 
-router.post('/', productController.create);
-
 router.get('/', productController.getAll);
+
+router.use(validateProduct);
+router.post('/', productController.create);
 
 export default router;
