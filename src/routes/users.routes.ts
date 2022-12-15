8e552851aae7d70c8e4fba181d5d3a2875629869
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-12-15 12:58:29 -0300

    :sparkles: feat: Requisito 7 - Criando validações para cadastro de usuarios

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,21 @@
 import express from 'express';
 import usersControllers from '../controllers/users.controllers';
+import {
+  validateLevel,
+  validatePassword,
+  validateUsername,
+  validateVocation,
+} from '../middleware/validateUser';
 
 const userRouters = express.Router();
 
-userRouters.post('/', usersControllers.createUser);
+userRouters.post(
+  '/', 
+  validateLevel,
+  validatePassword,
+  validateUsername,
+  validateVocation,
+  usersControllers.createUser,
+);
 
 export default userRouters;
