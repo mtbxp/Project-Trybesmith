4115802bbb33abec19a2e1d4252a82e42842d110
Vinny-knzts <viniciusmasouza@hotmail.com>
2022-12-14 20:14:15 -0400

    Completa req 01.

---
+++ src/services/products.service.ts
@@ -0,0 +1,16 @@
+import connection from '../models/connection';
+import ProductModel from '../models/products.model';
+import Product from '../interfaces/products.interface';
+
+export default class ProductService {
+  public model: ProductModel;
+
+  constructor() {
+    this.model = new ProductModel(connection);
+  }
+
+  public async create(product: Product): Promise<Product> {
+    const newProduct = await this.model.create(product);
+    return newProduct;
+  }
+}
