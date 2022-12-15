dca0fa8a51fc6e3de8f6b662d24145ad262d07ed
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-15 09:11:14 -0300

    arruma erro de query

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -4,10 +4,10 @@ import { TOrders } from '../types';
 
 export async function getAll():Promise<TOrders[]> {
   const [result] = await connection.execute<RowDataPacket[] &
-  TOrders[]>(`SELECT odr.id, odr.user_id AS userId, JSON_ARRAYAGG(P.order_id) AS productsIds 
+  TOrders[]>(`SELECT odr.id, odr.user_id AS userId, JSON_ARRAYAGG(P.id) AS productsIds 
   FROM Trybesmith.orders AS odr 
   INNER JOIN Trybesmith.products AS P 
-  ON odr.id = P.order_id group by odr.id`);
+  ON odr.id = P.order_id group by P.order_id`);
   return result;
 }
 
