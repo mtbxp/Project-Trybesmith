337f2fd303e343fff6fd8f905a41694804d51a73
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2022-12-15 11:55:33 -0300

    feat: validations for endpoint post /users

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import Controller from '../controller';
+import Middleware from '../middleware/User';
 
 const router = Router();
 const controller = new Controller.User();
 
-router.post('/', controller.post.bind(controller));
+router.post('/', Middleware.validateUser, controller.post.bind(controller));
 
 export default router;
