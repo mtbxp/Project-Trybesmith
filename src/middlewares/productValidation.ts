493c0e397b1b07c5f17d7399d3e94f6a62bb7ccd
root <testepa45@gmail.com>
2022-12-16 18:43:28 +0000

    requisito-06

---
+++ src/middlewares/productValidation.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+/* import { status } from '../statusCode/status'; */
+
+export default function validationName(req: Request, res: Response, next: NextFunction) {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+  if (typeof name !== 'string') {
+    return res.status(422).json({ message: '"name" must be a string' });
+  }
+
+  if (name.length < 3) {
+    return res.status(422).json({ message: '"name" length must be at least 3 characters long' });
+  }
+
+  return next();
+}
--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import * as productsController from '../controllers/productsController';
+import validationName from '../middlewares/productValidation';
+import validationAmount from '../middlewares/productValidation2';
 
 const router = Router();
 
-router.post('/', productsController.insertProductC);
+router.post('/', validationName, validationAmount, productsController.insertProductC);
 router.get('/', productsController.getAllProductC);
 
 export default router;
