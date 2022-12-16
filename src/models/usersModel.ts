be25757450bf5002fd9ea8c230884b4748191b6f
root <testepa45@gmail.com>
2022-12-16 00:02:56 +0000

    .

--- src/models/usersModel.ts
+++ src/models/usersModel.ts
@@ -2,21 +2,18 @@ import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { TUser } from '../types';
 import connection from './connection';
 
-export async function insert(user: TUser): Promise<void> {
+export async function insert(user: TUser): Promise<TUser[]> {
   const { username, vocation, level, password } = user;
   await connection.execute<ResultSetHeader>(
     'INSERT INTO Trybesmith.users (username, vocation, level, password) VALUES (?, ?, ?, ?)',
     [username, vocation, level, password],
   );
-}
-
-export const searchUser = async (name: string): Promise<TUser[]> => {
   const [posts] = await connection
     .execute<RowDataPacket[]>(
     'SELECT id, username FROM Trybesmith.users WHERE username = ?', 
-    [name],
+    [username],
   );
   return posts as TUser[];
-};
+}
 
 export default insert;
--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -3,14 +3,8 @@ import { createToken } from '../auth/createToken';
 import * as usersModel from '../models/usersModel';
 
 export async function insertUserS(user: TUser): Promise<string> {
-  const { username } = user;
-
-  const result = await usersModel.searchUser(username);
-
-  await usersModel.insert(user);
-
+  const result = await usersModel.insert(user);
   const token = createToken(result);
-  
   return token;
 }
 
