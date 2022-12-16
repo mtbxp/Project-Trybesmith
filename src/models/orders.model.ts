7efb0e736527249c7e7267071048da868cb284f0
levyhb <levybholanda@gmail.com>
2022-12-16 16:12:01 -0300

    fix: refactoring order model

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -2,27 +2,15 @@ import { RowDataPacket } from 'mysql2';
 import connection from './connection';
 import { Orders } from '../interfaces/interfaces';
 
-const getProducts = async (id: number):Promise<number[]> => {
-  const [products] = await connection
-    .execute<RowDataPacket[] & number[]>(`SELECT id FROM Trybesmith.products
-    WHERE order_id = ?`, [id]);
-  const list = products.map((p) => p.id);
-  return list;
-};
-
 const getAllOrders = async () => {
   const [orders] = await connection
-    .execute<RowDataPacket[]>('SELECT * FROM Trybesmith.orders;');
-  const ordersWithProducts = await Promise.all(orders.map(async (order): Promise<Orders> => { 
-    const products = await getProducts(order.id);
-    await Promise.all(products);
-    return {
-      id: order.id,
-      userId: order.user_id,
-      productsIds: products,
-    };
-  }));
-  return ordersWithProducts;
+    .execute<RowDataPacket[] & Orders[]>(`SELECT ord.id as id, ord.user_id as userId, 
+    JSON_ARRAYAGG(p.id) as productsIds
+    FROM Trybesmith.orders as ord
+    INNER JOIN Trybesmith.products as p
+    ON p.order_id = ord.id
+    GROUP BY ord.id;`);
+  return orders;
 };
 
 export default {
