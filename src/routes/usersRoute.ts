5f18711cbbf2b43143e03d54342a2f5a950fc453
root <testepa45@gmail.com>
2022-12-16 19:54:06 +0000

    requisito-07

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,8 +1,20 @@
 import { Router } from 'express';
 import * as usersController from '../controllers/usersController';
+import validationLevel from '../middlewares/userLevelValidation';
+import validationUserName from '../middlewares/userNameValidation';
+import validationPassword from '../middlewares/userPasswordValidation';
+import validationVocation from '../middlewares/userVocationValidation';
 
 const router = Router();
 
-router.post('/', usersController.insertUserC);
+router.post(
+  '/', 
+ 
+  validationUserName, 
+  validationPassword,
+  validationLevel,
+  validationVocation,
+  usersController.insertUserC,
+);
 
 export default router;
