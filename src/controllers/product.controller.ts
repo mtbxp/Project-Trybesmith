f947e024b9dbe5ea4474b8b54bb2844c921151fc
Ludson96 <ludson_ps27@hotmail.com>
2022-12-18 18:26:10 -0300

    add controller createProduct

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import ProductService from '../services/product.service';
+import statusCodes from '../utils/statusCodes';
+
+export default class ProductController {
+  constructor(private productService = new ProductService()) { }
+
+  public createProduct = async (req: Request, res: Response) => {
+    try {
+      const input = req.body;
+      const newProduct = await this.productService.createProdutc(input);
+      return res.status(statusCodes.CREATED).json(newProduct);
+    } catch (e) {
+      const erro = (e as Error).message;
+      return res.status(statusCodes.INTERNAL_ERROR).json({
+        message: 'Não foi possivel cadastrar o produto',
+        erro,
+      });
+    }
+  };
+}
