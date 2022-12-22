d83517b4db68df8673a21305f950980e1cdd3bcc
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2022-12-22 16:31:35 -0300

    req4

--- src/models/order.models.ts
+++ src/models/order.models.ts
@@ -3,13 +3,16 @@ import connection from './connection';
 import { Order } from '../interfaces/interface';
 
 async function getAllOrders(): Promise<Order[]> {
-  const [result] = await connection.execute<RowDataPacket[] & Order[]>(
-    'SELECT * FROM Trybesmith.orders',
-  );
-
   /* const [result] = await connection.execute<RowDataPacket[] & Order[]>(
-    'SELECT id, user_id, (SELECT JSON_ARRAYAGG(order_id)) FROM Trybesmith.orders',
+    'SELECT * FROM Trybesmith.orders',
   ); */
+  const [result] = await connection.execute<RowDataPacket[] & Order[]>(
+    `SELECT orders.id AS id, users.id AS userId, JSON_ARRAYAGG(products.id) AS productsIds
+     FROM Trybesmith.orders AS orders
+     INNER JOIN Trybesmith.products AS products ON orders.id = products.order_id
+     INNER JOIN Trybesmith.users AS users ON orders.user_id = users.id
+     GROUP BY products.order_id;`,
+  );
   return result;
 }
 
