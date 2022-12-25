87abdfd54fc0a9e61cf27c12558b002753a5c7d4
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-25 12:21:29 -0300

    feat: model of add products and interface

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,1 +1,14 @@
-// import connection from './connection';
+import { ResultSetHeader } from 'mysql2';
+import connection from './connection';
+import { IProducts, NewProduct } from '../interfaces/Product.interface';
+
+const addProduct = async (product: NewProduct): Promise<IProducts> => {
+  const q = 'INSERT INTO Trybesmith.products (name, amount) VALUES (?,?)';
+  const [response] = await connection.execute<ResultSetHeader>(q, [product.name, product.amount]);
+  const result = { id: response.insertId, ...product };
+  return result;
+};
+
+export default {
+  addProduct,
+};
