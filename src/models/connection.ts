0909c7c53ca3d61833f8d5762462c3575b77b089
pguilhermef <pguilhermesantos@hotmail.com>
2022-12-26 16:34:35 -0300

    feat: Tipa o connection e remove comentário inútil

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,12 +1,10 @@
-import dotenv from 'dotenv';
-import mysql from 'mysql2/promise';
+import 'dotenv/config';
+import mysql, { Pool } from 'mysql2/promise';
 
-dotenv.config();
-
-const connection = mysql.createPool({
+const connection: Pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-}); // sua conexão NÃO deve ter o database, este deve ser especificado em cada query
+});
 
 export default connection;
