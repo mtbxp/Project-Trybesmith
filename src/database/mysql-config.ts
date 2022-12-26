f104d77b56cca6e0f682b44da3b3accf384358f4
Ruham Leal <ruhamxlpro@hotmail.com>
2022-12-26 08:54:34 -0300

    chore: configuring tsconfig

--- src/database/mysql-config.ts
+++ src/database/mysql-config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connection = createPool({
   host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 3306,
+  port: 3306,
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '123456',
   database: process.env.MYSQL_DATABASE || 'TrybeSmith',
