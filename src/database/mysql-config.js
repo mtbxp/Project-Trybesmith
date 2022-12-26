c561694f31b959f3c73e233ba7d61ebb53cb0882
Ruham Leal <ruhamxlpro@hotmail.com>
2022-12-26 08:35:09 -0300

    chore: configuring mysql database

---
+++ src/database/mysql-config.js
@@ -0,0 +1,14 @@
+import { createPool } from 'mysql2/promise';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const connection = createPool({
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || 3306,
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '123456',
+  database: process.env.MYSQL_DATABASE || 'TrybeSmith',
+});
+
+export default connection;
