b490ffba9e1bf51e57404a7e71de7bf2be92e70c
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-26 22:12:46 -0300

    feat: model of getall

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -2,13 +2,21 @@ import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
 import { IProducts, NewProduct } from '../interfaces/Product.interface';
 
-const addProduct = async (product: NewProduct): Promise<IProducts> => {
-  const q = 'INSERT INTO Trybesmith.products (name, amount) VALUES (?,?)';
-  const [response] = await connection.execute<ResultSetHeader>(q, [product.name, product.amount]);
+const addProduct = async (product: NewProduct): Promise<IProducts> => { 
+  const query = 'INSERT INTO Trybesmith.products (name, amount) VALUES (?,?)';
+  const [response] = await connection
+    .execute<ResultSetHeader>(query, [product.name, product.amount]);
+  
   const result = { id: response.insertId, ...product };
   return result;
 };
 
+const getAll = async (): Promise<IProducts[]> => {
+  const [result] = await connection.execute('SELECT * FROM Trybesmith.products;');
+  return result as IProducts[];
+};
+
 export default {
   addProduct,
+  getAll,
 };
