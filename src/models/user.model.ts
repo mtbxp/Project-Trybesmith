1a7461260c22c1c9d34f6af029200506096ea0a2
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-27 12:48:42 -0300

    feat: create model of user to add new user

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,1 +1,18 @@
-// import connection from './connection';
+import { ResultSetHeader } from 'mysql2/promise';
+import connection from './connection';
+import { AddUser, User } from '../interfaces/User.interface';
+
+const createUser = async (user: AddUser): Promise<User> => {
+  const { username, vocation, level, password } = user;
+  const qr = 'INSERT INTO Trybesmith.users (username, password, level, vocation) VALUES (?,?,?,?);';
+  const [result] = await connection
+    .execute<ResultSetHeader>(qr, [username, vocation, level, password]);
+    
+  const { insertId } = result;
+
+  return { id: insertId, username, level, vocation };
+};
+
+export default {
+  createUser,
+};
