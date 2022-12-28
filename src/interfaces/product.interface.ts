29d286b2676a6590b9df0d95e2a82a228820725c
pguilhermef <pguilhermesantos@hotmail.com>
2022-12-28 18:20:57 -0300

    refactor: Retira a redundancia da interface de produto

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -1,10 +1,8 @@
-export interface Product {
-  id: number,
+export interface ProductParameters {
   name: string,
   amount: number,
 }
 
-export interface ProductParameters {
-  name: string,
-  amount: number,
+export interface Product extends ProductParameters {
+  id: number,
 }
