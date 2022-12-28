f5ac3e48518845dbc235675968ec04082bc9bbec
Caio <caiusito@gmail.com>
2022-12-28 09:30:50 -0300

    Organizando Query Req 4

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -4,7 +4,12 @@ import connection from './connection';
 // eslint-disable-next-line import/prefer-default-export
 export async function getAll(): Promise<Iorders[]> {
   const [orders] = await connection.execute(
-    'SELECT * FROM Trybesmith.orders',
+    `SELECT Trybesmith.orders.id, Trybesmith.orders.user_id AS userId, 
+    JSON_ARRAYAGG(Trybesmith.products.id)
+    FROM Trybesmith.orders
+    INNER JOIN Trybesmith.products
+    WHERE Trybesmith.orders.id = Trybesmith.products.order_id
+    GROUP BY Trybesmith.orders.id;`,
   );
   return orders as Iorders[];
 }
