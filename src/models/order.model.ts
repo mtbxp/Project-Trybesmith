8631ab460588a4cfd7c313bbfb21cd23d64a7384
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-27 21:57:39 -0300

    feat: model to get orders

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,1 +1,20 @@
-// import connection from './connection';
+import { RowDataPacket } from 'mysql2/promise';
+import { Order } from '../interfaces/Order.interface';
+import connection from './connection';
+
+const getAll = async (): Promise<Order[]> => {
+  const [result] = await connection.execute<RowDataPacket[] & Order[]>(
+    `
+    SELECT orders.id,
+    orders.user_id AS userId,
+    JSON_ARRAYAGG(products.id) AS productsIds 
+    FROM Trybesmith.orders AS orders
+    INNER JOIN Trybesmith.products AS products
+    ON orders.id = products.order_id
+    GROUP BY orders.id; 
+    `,
+  );
+  return result;
+};
+
+export default { getAll };
