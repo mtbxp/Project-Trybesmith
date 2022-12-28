2d0c10b04db6142daf5ca777d52c26e5cc79353e
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-27 21:59:17 -0300

    feat: finalit route to get orders

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import product from './routes/product.route';
 import user from './routes/user.route';
+import orders from './routes/order.route';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/products', product);
 app.use('/users', user);
+app.use('/orders', orders);
 
 export default app;
