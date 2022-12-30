458d49e4f055f25a93673a60587cbd1ab26b864a
pguilhermef <pguilhermesantos@hotmail.com>
2022-12-30 17:19:55 -0300

    feat: Move a desestruturação dos parametros  de createProductModel para dentro da função

--- src/models/product.models.ts
+++ src/models/product.models.ts
@@ -2,7 +2,9 @@ import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
 import { Product, ProductParameters } from '../interfaces/product.interface';
 
-const createProductModel = async ({ name, amount }: ProductParameters): Promise<Product> => {
+const createProductModel = async (product: ProductParameters): Promise<Product> => {
+  const { name, amount } = product;
+
   const [result] = await connection.execute<ResultSetHeader>(
     'INSERT INTO Trybesmith.products (name, amount) VALUES (?, ?);',
     [
