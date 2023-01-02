116305bc58f8aa2243bf3a2dbc5f40f150946f53
wan lucas <wan.lucas.90@gmail.com>
2023-01-02 16:56:34 -0300

    :sparkles: criação de controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,9 @@
+import usersController from './users.controller';
+import productsController from './products.controller';
+import ordersController from './orders.controller';
+
+export default {
+  usersController,
+  productsController,
+  ordersController,
+};
