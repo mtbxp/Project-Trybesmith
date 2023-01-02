5559dc2a41740b93dac1539fe017bf4334ff6931
wan lucas <wan.lucas.90@gmail.com>
2023-01-02 17:09:37 -0300

    :sparkles: criação de services

---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+import usersService from './users.service';
+import productsService from './products.service';
+import ordersService from './orders.service';
+
+export default {
+  usersService,
+  productsService,
+  ordersService,
+};
