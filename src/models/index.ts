92e250d42ae6ff12d6aa31177fa2a97a17f3908e
wan lucas <wan.lucas.90@gmail.com>
2023-01-02 17:35:44 -0300

    :sparkles: criação de models

---
+++ src/models/index.ts
@@ -0,0 +1,9 @@
+import usersModel from './users.model';
+import productsModel from './products.model';
+import ordersModel from './orders.model';
+
+export default {
+  usersModel,
+  productsModel,
+  ordersModel,
+};
