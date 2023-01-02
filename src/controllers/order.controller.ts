6bfbfe1606fe492f3454e08d468378789a917fd5
pguilhermef <pguilhermesantos@hotmail.com>
2023-01-01 21:43:25 -0300

    feat: Cria controller de order

---
+++ src/controllers/order.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import orderService from '../services/orders.service';
+
+const getAllOrdersController = async ( _req: Request, res: Response,) => {
+  const allOrders = await orderService.getAllOrdersService();
+  return res.status(200).json(allOrders);
+};
+
+export default {
+  getAllOrdersController,
+};
