a251302370428a3ff290e83a637cacb4daefa4e1
wan lucas <wan.lucas.90@gmail.com>
2023-01-03 16:06:24 -0300

    :sparkles: error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express from 'express';
+import 'express-async-errors';
+import express, { Request, Response, NextFunction } from 'express';
 import routers from './routers';
+import HttpError from './utils/errors';
 
 const app = express();
 
@@ -8,5 +10,9 @@ app.use(express.json());
 app.use(routers.usersRouter);
 app.use('/products', routers.productsRouter);
 app.use('/orders', routers.ordersRouter);
+
+app.use((error: HttpError, _req: Request, res: Response, _next: NextFunction) => {
+  res.status(error.statusCode).json({ message: error.message });
+});
 
 export default app;
