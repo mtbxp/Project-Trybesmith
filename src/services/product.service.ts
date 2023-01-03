690836fe92cfea829c9f81d101d691720efb9fb8
Matthews Bastos <matthewsbasttos@gmail.com>
2023-01-03 10:04:49 -0300

    Adiciona o método getAllProducts para a camada service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -9,6 +9,11 @@ class ProductService {
     this.model = new ProductModel(connection);
   }
 
+  public async getAllProducts(): Promise<Product[]> {
+    const products = await this.model.getAllProducts();
+    return products;
+  }
+
   public async createProduct(product: Product): Promise<Product> {
     return this.model.createProduct(product);
   }
