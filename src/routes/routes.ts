3a5c3adf3271f3d2aed9b1a97dfadfc488f09334
Rafael Miranda <35074501+rafamirandaa@users.noreply.github.com>
2023-01-03 21:29:34 -0300

    requisito 7 resolvido

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,20 +3,28 @@ import productsController from '../controller/products.controller';
 import usersController from '../controller/users.controller';
 import ordersController from '../controller/orders.controller';
 import loginController from '../controller/login.controller';
-import middlewares from '../middlewares/verifyNameAndAmount.middleware';
+import middlewaresProducts from '../middlewares/verifyNameAndAmount.middleware';
+import middlewareUsers from '../middlewares/verifyUsernameVocationLevelPassword.middleware';
 
 const router = express.Router();
 
 router.post(
   '/products', 
-  middlewares.verifyName,
-  middlewares.verifyAmount, 
+  middlewaresProducts.verifyName,
+  middlewaresProducts.verifyAmount, 
   productsController.createProductController,
 );
 
 router.get('/products', productsController.getAllProductsController);
 
-router.post('/users', usersController.createUserController);
+router.post(
+  '/users',
+  middlewareUsers.verifyUsername,
+  middlewareUsers.verifyVocation,
+  middlewareUsers.verifyLevel,
+  middlewareUsers.verifyPassword,
+  usersController.createUserController,
+);
 
 router.get('/orders', ordersController.createOrderController);
 
