73faa7fed2b96bdcb11d86a0cb2768bdb42d64ed
guigabriel <guilhermelisboa98@outlook.com>
2023-01-06 15:55:55 -0300

    Fix: lint

--- src/controllers/login.controlle.ts
+++ src/controllers/login.controlle.ts
@@ -4,9 +4,11 @@ import loginService from '../service/login.service';
 const login = async (req: Request, res: Response) => {
   const { body } = req;
   const result = await loginService.login(body);
-  if (result.status === 401) return res.status(401).json({
-    message:result.message,
-  });
+  if (result.status === 401) {
+    return res.status(401).json({
+      message:result.message,
+   });
+  } 
   res.status(200).json({ token: result.token });
 };
 
