f54430cdb406c8ea2fe3393453fcf90565ef350d
karina <lealk530@gmail.com>
2023-01-11 22:47:50 -0300

    Iniciando requesito 01

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import registerProducts from './routes/registerProducts';
 
 const app = express();
 
 app.use(express.json());
+app.use('/products', registerProducts);
 
 export default app;
---
+++ src/routes/registerProducts.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const registerProducts = Router();
+
+export default registerProducts;
