c9ccbcaceaf5d7888135a693cc05a4710df4284e
xWesleyMartins <wesley_869@hotmail.com.br>
2023-01-13 18:36:20 -0300

    concertando req03 e deletando arquivos desnecessarios.

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -9,7 +9,7 @@ export default class UserModel {
     const { username, vocation, level, password } = user;
     const [{ insertId }] = await this
       .connection.execute<ResultSetHeader>(` INSERT INTO
-        Trybesmith.Users (username, vocation, level, password)
+        Trybesmith.users (username, vocation, level, password)
         VALUES(?, ?, ?, ?)`, [username, vocation, level, password]);
     return { id: insertId, username, vocation, level, password };
   }
