a728a4cd62fd521174e7af45906271cc35bcb9e4
Gabriel <gabrielalves3654@gmail.com>
2023-01-13 16:04:17 -0300

    req 1

--- src/controllers/product.controllers.ts
+++ src/controllers/product.controllers.ts
@@ -5,7 +5,14 @@ const getAll = async (_req: Request, res: Response):Promise<void> => {
   const products = await productsService.getAll();
   res.status(200).json(products);
 };
+
+const create = async (req: Request, res: Response):Promise<void> => {
+  const product = req.body;
+  const productCreated = await productsService.create(product);
+  res.status(201).json(productCreated);
+};
   
 export default {
   getAll,
+  create,
 };
--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,13 +1,21 @@
-import { RowDataPacket } from 'mysql2';
+import { RowDataPacket, ResultSetHeader } from 'mysql2';
 import connection from './connection';
-import { TProducts } from '../types/Types';
+import { TProduct, NewProduct } from '../types/Types';
 
-const getAll = async (): Promise<TProducts[]> => {
+const getAll = async (): Promise<TProduct[]> => {
   const [products] = await connection
-    .execute<RowDataPacket[] & TProducts[]>('SELECT * FROM Trybesmith.products;');
+    .execute<RowDataPacket[] & TProduct[]>('SELECT * FROM Trybesmith.products;');
   return products;
+};
+const create = async (product: NewProduct): Promise<TProduct> => {
+  const query = 'INSERT INTO Trybesmith.products (name, amount) VALUES (?,?)';
+  const [response] = await connection
+    .execute<ResultSetHeader>(query, [product.name, product.amount]);
+  const productCreated = { id: response.insertId, ...product }; 
+  return productCreated;
 };
 
 export default {
   getAll,
+  create,
 };
