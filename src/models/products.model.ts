13d034e8bd4676121274fff6e65873ac7d62b907
luizfilipelgs <luizfilipelgs@gmail.com>
2023-01-12 23:06:38 -0300

    feat: getAllProducts model

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,13 +1,19 @@
-import { ResultSetHeader } from 'mysql2';
-import { Product, ProductR } from '../interfaces/Product';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import { Product, ProductOrd, ProductR } from '../interfaces/Product';
 import connections from './connection';
 
 const addNewProduct = async (product: Product): Promise<ProductR> => {
   const [{ insertId }] = await connections.execute<ResultSetHeader>(`
-    INSERT INTO Trybesmith.products (name) VALUES (?, ?)`, [product.name, product.amount]);
+    INSERT INTO Trybesmith.products (name, amount) VALUES (?,?)`, [product.name, product.amount]);
   return { id: insertId, ...product };
+};
+
+const getAllProducts = async (): Promise<ProductOrd[]> => {
+  const [result] = await connections.execute('SELECT * FROM Trybesmith.products ORDER BY id');
+  return result as ProductOrd[]; // why type at the end again ?
 };
 
 export default {
   addNewProduct,
+  getAllProducts,
 };
