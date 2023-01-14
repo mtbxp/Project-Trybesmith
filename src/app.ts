a102c23b26baec9df4d94240e69721ca1a4c3bce
Sheifer Kepler <sheifer.kda@gmail.com>
2023-01-14 14:20:52 -0300

    feat: route getAll products done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import productsControllers from './controllers/products.controllers';
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/products', productsControllers.getAll);
 
 export default app;
