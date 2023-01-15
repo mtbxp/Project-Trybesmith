f16088edacc140e151ec91fdf84f2654be6c5b07
Julia Peres Kitzberger <xjujuperesx@hotmail.com>
2023-01-15 20:02:23 -0300

    Corrigindo a rota order e adicionando model, service, controller e interface ORDER

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { application } from 'express';
 import productsRouter from './router/productsRouter';
 import usersRouter from './router/usersRouter';
 import orderRouter from './router/orderRouter';
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
-app.use('(/orders', orderRouter);
+app.use('/orders', orderRouter);
+// app.use('/login', loginRouter);
 
 export default app;
