df5204b85b067650e2cc24eff277b9da4786d50e
lorena-mendes <lorena.lccma@gmail.com>
2023-01-15 09:32:52 -0300

    Requisito 7 faltando uma validação

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import usersController from '../controllers/users.controller';
+import usersMiddlewares from '../middlewares/users.middlewares';
 
 const router = Router();
 
-router.post('/', usersController.registerUser);
+router.post(
+  '/',
+  usersMiddlewares.validateUsername,
+  usersMiddlewares.validateVocation,
+  usersMiddlewares.validateLevel,
+  usersMiddlewares.validatePassword,
+  usersController.registerUser,
+);
 router.get('/', usersController.findAllUsers);
 
 export default router;
