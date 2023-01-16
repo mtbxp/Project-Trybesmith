1d1ea77059ca0ffbb99064df562b4c3cb42aab9a
Sheifer Kepler <sheifer.kda@gmail.com>
2023-01-16 12:07:38 -0300

    feat: route register users done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import productsControllers from './controllers/products.controllers';
 import ordersControllers from './controllers/orders.controllers';
+import usersControllers from './controllers/users.controllers';
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 app.get('/products', productsControllers.getAll);
 app.post('/products', productsControllers.insert);
 app.get('/orders', ordersControllers.getAll);
+app.post('/users', usersControllers.insert);
 
 export default app;
