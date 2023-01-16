53c2ccfe031d9ca1774c439358915016f1d7f985
Sheifer Kepler <sheifer.kda@gmail.com>
2023-01-16 12:13:40 -0300

    feat: messages

---
+++ src/utils/messages.ts
@@ -0,0 +1,3 @@
+export const requiredUser = '"username" is required';
+export const requiredPassword = '"password" is required';
+export const invalidUserOrPassword = 'Username or password invalid';
