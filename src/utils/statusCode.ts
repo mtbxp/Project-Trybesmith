6adcae79e646570190cc38fdc255b03cc5b92c8b
luizfilipelgs <luizfilipelgs@gmail.com>
2023-01-16 22:11:36 -0300

    fix: clean code statusCode

--- src/utils/statusCode.ts
+++ src/utils/statusCode.ts
@@ -1,15 +1,3 @@
-/* const statusCode = {
-  'any.required': 400,
-  'string.empty': 400,
-  'number.min': 422,
-  'string.min': 422,
-  'string.base': 422,
-  'number.exist': 404,
-}; */
-
-/* console.log(statusCode['any.required']);
-
- */
 const statusCode = (error: string): number => {
   if (error === 'any.required') return 400;
   return 422;
