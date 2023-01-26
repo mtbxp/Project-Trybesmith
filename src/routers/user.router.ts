3dfa08be3220ca218393d53bf777c9db3a7a065f
mtbxp <marcellotb2@gmail.com>
2023-01-26 18:55:43 -0300

    Add User Validations

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,12 +1,20 @@
 import { Router } from 'express';
 import UsersController from '../controllers/users.controller';
 import loginValidation from '../middleware/loginAuth';
+import UserValidation from '../middleware/userValidation';
 
 const router = Router();
 
 const usersController = new UsersController();
 
-router.post('/users', usersController.create);
+router.post(
+  '/users', 
+  UserValidation.usernameValidation,
+  UserValidation.vocationValidation,
+  UserValidation.levelValidation,
+  UserValidation.passwordValidation,
+  usersController.create,
+);
 router.post('/login', loginValidation, usersController.login);
 
 export default router;
