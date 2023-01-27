030be6eab5fe9abc0fb63900872b5d8d17bf91c1
Marcos Leite <marcosvpleite@gmail.com>
2023-01-27 14:24:49 -0300

    requisito 7 realizado

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import usersController from '../controllers/users.controller';
+import validateUsers from '../middlewares/validateUsers.middlewares';
 
 const router: Router = Router();
 
-router.post('/', usersController.create);
+router.post(
+  '/',
+  validateUsers.validateUsername,
+  validateUsers.validateVocation,
+  validateUsers.validateLevel,
+  validateUsers.validatePassword,
+  usersController.create,
+);
 
 export default router;
