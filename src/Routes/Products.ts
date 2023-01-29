8d0e37b2a966c719b20bcfad520493681be810ef
lucasbarreto92 <lucasbs.code@gmail.com>
2023-01-28 21:32:58 -0300

    REQUISITO 06 - Cria as validações dos produtos

--- src/Routes/Products.ts
+++ src/Routes/Products.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import ProductsController from '../Controllers/ProductsController';
+import validate from '../Middlewares/Product';
 
 const productsRouter = express.Router();
 
 const productsController = new ProductsController();
 
 // REQUISITO 01
-productsRouter.post('/', productsController.insert);
+productsRouter.post('/', validate, productsController.insert);
 
 // REQUISITO 02
 productsRouter.get('/', productsController.getAll);
