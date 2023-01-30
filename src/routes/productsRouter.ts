aa246a73e5f6dabde40abf066b02669c034d9da2
JoyceTeofilo <joyceteofilo@gmail.com>
2023-01-30 07:33:48 -0300

    Requisito 06

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import productsController from '../controllers/productsController';
+import { nameValidate, amountValidate } from '../middlewares/productsValidations';
 
 const router = Router();
 
 router.get('/products', productsController.getAllProducts);
-router.post('/products', productsController.createProduct);
+router.post('/products', nameValidate, amountValidate, productsController.createProduct);
 
 export default router;
