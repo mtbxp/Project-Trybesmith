67cca9a1556d574c9b353936ce952dec33cd5ed1
Gabriel Cantuária <gabriel13n@gmail.com>
2023-01-30 20:54:26 -0300

    requisito 6

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import productsController from '../controllers/products.controller';
+import { validateAmout, validateName } from '../middlewares/validateProducts';
 
 const router = Router();
 
 router.get('/products', productsController.getProducts);
-router.post('/products', productsController.registerProduct);
+router.post('/products', validateName, validateAmout, productsController.registerProduct);
 
 export default router;
