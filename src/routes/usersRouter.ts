b40444ee19b68b4f79800e8262b4d006d866ad1a
JoyceTeofilo <joyceteofilo@gmail.com>
2023-01-30 16:59:24 -0300

    Requisito 07

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express';
 import usersController from '../controllers/usersController';
+import { levelValidate, passwordValidate, usernameValidate,
+  vocationValidate } from '../middlewares/usersValidations';
 
 const router = Router();
 
-router.post('/users', usersController.createUser);
+router.post(
+  '/users',
+  usernameValidate,
+  vocationValidate,
+  levelValidate,
+  passwordValidate,
+  usersController.createUser,
+);
 
 export default router;
