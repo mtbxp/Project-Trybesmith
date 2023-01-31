83c32e8e3c278db4155cb2d016916893622fb5c3
pamelamonteiro <pamelamonteiro_@hotmail.com>
2023-01-31 00:46:33 -0300

    Requisito 7

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import usersController from '../controllers/usersController';
+import usersMiddlewares from '../middlewares/usersMiddlewares';
 
 const usersRouter = Router();
 
-usersRouter.post('/', usersController.addUser);
+usersRouter.post(
+  '/',
+  usersMiddlewares.isUsernameValid,
+  usersMiddlewares.isVocationValid,
+  usersMiddlewares.isLevelValid,
+  usersMiddlewares.isPasswordValid,
+  usersController.addUser,
+);
 
 export default usersRouter;
