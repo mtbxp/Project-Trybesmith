ce0808d127f9971192ae99a47f1b282938944bb7
Ton Lima <tondrums@hotmail.com>
2023-01-31 00:20:46 -0300

    Req 07- criando validações para as pessoas usuárias

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import UserController from '../controller/usersController';
+import ValidateUsers from '../middlewares/validateUsers';
 
 const router = Router();
 
 const userController = new UserController();
+const validateUsers = new ValidateUsers();
 
-router.post('/', userController.userCreated);
+router.post(
+  '/',
+  validateUsers.nameValidated,
+  validateUsers.validatedLevel,
+  validateUsers.validatedPassword,
+  validateUsers.validatedVocation,
+  userController.userCreated,
+);
 
 export default router;
