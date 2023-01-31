4dfdbc12060f20981102f741ababb3a5c0a23120
pamelamonteiro <pamelamonteiro_@hotmail.com>
2023-01-31 12:36:47 -0300

    Testando o req 8 novamente

--- src/middlewares/ordersMiddlewares.ts
+++ src/middlewares/ordersMiddlewares.ts
@@ -1,26 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
-// import { verify } from 'jsonwebtoken';
+import { verify } from 'jsonwebtoken';
 
-// const secret = process.env.JWT_SECRET || 'secret';
+const secret = process.env.JWT_SECRET || 'secret';
 
-// const verifyToken = (token: string) => {
-//   console.log(token, secret);
+const verifyToken = (token: string) => {
+  console.log(token, secret);
   
-//   const checkToken = verify(token, secret);
-//   console.log(checkToken);
+  const checkToken = verify(token, secret);
+  console.log(checkToken);
   
-//   return checkToken as object;
-// };
+  return checkToken as object;
+};
 
 const isTokenValid = async (req: Request, res: Response, next: NextFunction) => {
-  // const { authorization: token } = req.headers;
-  // if (!token) return res.status(401).json({ message: 'Token not found' });
-  // try {
-  //   verifyToken(token);
-  //   next();
-  // } catch (error) {
-  //   return res.status(401).json({ message: 'Invalid token' });
-  // }
+  const { authorization: token } = req.headers;
+  if (!token) return res.status(401).json({ message: 'Token not found' });
+  try {
+    verifyToken(token);
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
   next();
 };
 
