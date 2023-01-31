0449a8a48447fa5a0c9355a1509649db80cc9553
Gabriel Cantuária <gabriel13n@gmail.com>
2023-01-30 21:15:23 -0300

    requisito 7

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,8 +1,21 @@
 import { Router } from 'express';
 import usersController from '../controllers/users.controller';
+import { 
+  validateLevel, 
+  validateVocation, 
+  validatePassword, 
+  validateUsername, 
+} from '../middlewares/validateUsers';
 
 const router = Router();
 
-router.post('/users', usersController.registerUser);
+router.post(
+  '/users', 
+  validateUsername, 
+  validateVocation, 
+  validateLevel, 
+  validatePassword, 
+  usersController.registerUser,
+);
 
 export default router;
