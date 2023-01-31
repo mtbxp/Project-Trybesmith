585f4361d7471cb4fc1dba908762a2ceac0ae828
Ton Lima <tondrums@hotmail.com>
2023-01-30 23:04:55 -0300

    Req 06- Criando validações dos produtos

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import ProductController from '../controller/productsController';
+import ValidateProducts from '../middlewares/validateProducts';
 
 const router = Router();
 
 const productsController = new ProductController();
+const validateProducts = new ValidateProducts();
 
 router.get('/', productsController.productsGetAll);
-router.post('/', productsController.productCreated);
+router.post(
+  '/', 
+  validateProducts.nameValidateProducts,
+  validateProducts.validateAmount, 
+  productsController.productCreated,
+);
 
 export default router;
