508088891d9ac19e439e308d39058fa1c97409d4
amadeus <amadeusdias@hotmail.com>
2023-01-31 05:38:30 -0300

    requisitos de validação, 6 e 7

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import productsControllers from '../controller/productsControllers';
+import productsMiddlewares from '../middlewares/productsMiddlewares';
 
 const router = Router();
 
-router.post('/', productsControllers.addProductController);
+router.post(
+  '/', 
+  productsMiddlewares.productsAmountValidation,
+  productsMiddlewares.productsNameValidation,
+  productsControllers.addProductController,
+);
+
 router.get('/', productsControllers.getAllProductsController);
 
 export default router;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import usersControllers from '../controller/userControllers';
+import usersMiddlewares from '../middlewares/usersMiddlewares';
 
 const router = Router();
 
-router.post('/', usersControllers.addUserController);
+router.post(
+  '/',
+  usersMiddlewares.passwordValidation,
+  usersMiddlewares.usernameValidation,
+  usersMiddlewares.vocationValidation,
+  usersMiddlewares.levelValidation,
+  usersControllers.addUserController,
+);
 
 export default router;
