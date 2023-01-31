c9bc23482637f6175afc60b444d384fc2ee28380
amadeus <amadeusdias@hotmail.com>
2023-01-31 01:56:19 -0300

    requisito 1 finalizado

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import productsControllers from '../controller/productsControllers';
 
 const router = Router();
+
+router.post('/', productsControllers.addProduct);
 
 export default router;
